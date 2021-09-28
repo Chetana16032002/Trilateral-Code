7f71b0b0f60838ca3f2de15da84b29a73c956f68
Masterbond7 <quintinslough@gmail.com>
2021-09-29 12:31:22 +1300

    testing

--- window-size.js
+++ window-size.js
@@ -5,6 +5,6 @@ function sidetable() {
     let width = $(window).width();
 
     // If the browser is big enough show the list of programming languages on the side
-    if (width < 625) { document.getElementById("prog-table-side").disabled=true; }
+    if (width < 625) { document.getElementById("prog-table-side").disabled=true; alert("smol");}
     else { document.getElementById("prog-table-side").disabled=false; }
 }
