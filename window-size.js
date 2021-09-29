ee214f25b4c3c40c5e524aef4dde578825576615
Masterbond7 <quintinslough@gmail.com>
2021-09-29 13:18:19 +1300

    fixed

--- window-size.js
+++ window-size.js
@@ -6,11 +6,6 @@ function sidetable() {
     let width = $(window).width();
 
     // If the browser is big enough show the list of programming languages on the side
-<<<<<<< HEAD
-    if (width < 625) { document.getElementById("prog-table-side").disabled=true; document.getElementById("no-prog-table-side").disabled=false; }
-    else { document.getElementById("prog-table-side").disabled=false; document.getElementById("no-prog-table-side").disabled=true; }
-=======
     if (width < 625) { document.getElementById("prog-table-side").disabled=true; alert("smol");}
     else { document.getElementById("prog-table-side").disabled=false; }
->>>>>>> parent of bb51364 (Cleaned up the fix)
 }
