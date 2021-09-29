f14279c423e5d6317eb5c4d444f2f22f15add5a4
Masterbond7 <quintinslough@gmail.com>
2021-09-29 14:15:05 +1300

    changed how squishey the website gets before the table moves down. also set a vertical minimum.

--- window-size.js
+++ window-size.js
@@ -4,9 +4,10 @@ window.addEventListener('load', sidetable);
 function sidetable() {
     // Get width and height of browser
     let width = $(window).width();
+    let height = $(window).height();
 
     // If the browser is big enough show the list of programming languages on the side
-    if (width < 625) { document.getElementById("prog-table-side").disabled=true;}
+    if (width < 1050 || height < 450) { document.getElementById("prog-table-side").disabled=true;} //MINIMUM WIDTH 625
     else { document.getElementById("prog-table-side").disabled=false; }
 }
 //comment
