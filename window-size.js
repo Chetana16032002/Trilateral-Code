c1f962a3f7050d90b2d3a162010fb8741353adaa
Masterbond7 <quintinslough@gmail.com>
2021-09-29 14:50:41 +1300

    Added a key to the list of programming languages

--- window-size.js
+++ window-size.js
@@ -7,7 +7,7 @@ function sidetable() {
     let height = $(window).height();
 
     // If the browser is big enough show the list of programming languages on the side
-    if (width < 1050 || height < 450) { document.getElementById("prog-table-side").disabled=true;} //MINIMUM WIDTH 625
+    if (width < 1050 || height < 680) { document.getElementById("prog-table-side").disabled=true;} //MINIMUM WIDTH 625  // TODO: IMPORTANT every time the side panel height is changed, the height value MUST be adjusted
     else { document.getElementById("prog-table-side").disabled=false; }
 }
 //comment
