9a6b0a438240ce8681ac360d74b9d3f872c10796
Masterbond7 <quintinslough@gmail.com>
2021-09-29 15:28:29 +1300

    Changed the window size requirements so it will load on chrome on a 768 height screen

--- window-size.js
+++ window-size.js
@@ -7,7 +7,7 @@ function sidetable() {
     let height = $(window).height();
 
     // If the browser is big enough show the list of programming languages on the side
-    if (width < 1050 || height < 680) { document.getElementById("prog-table-side").disabled=true;} //MINIMUM WIDTH 625  // TODO: IMPORTANT every time the side panel height is changed, the height value MUST be adjusted
+    if (width < 1050 || height < 625) { document.getElementById("prog-table-side").disabled=true;} //MINIMUM WIDTH 625  // TODO: IMPORTANT every time the side panel height is changed, the height value MUST be adjusted //680
     else { document.getElementById("prog-table-side").disabled=false; }
 }
 //comment
